62a50533f3f176f5c2c045c241fe2c357636a267
firecx <nikandrov.alekseix1@gmail.com>
2025-11-07 16:56:25 +0600

    clear test

--- src/test/java/org/firecx/server/ServerApplicationTests.java
+++ src/test/java/org/firecx/server/ServerApplicationTests.java
@@ -1,13 +1,8 @@
 package org.firecx.server;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
